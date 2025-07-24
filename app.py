c4e8dc77727470af472a1bf0fecb511b84d7fc4d
yaaabs <brianyabut02@gmail.com>
2025-07-25 00:07:34 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Ensure QR directory exists
+QR_FOLDER = 'static/qr'
+os.makedirs(QR_FOLDER, exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    qr_img_path = None
+    if request.method == 'POST':
+        data = request.form.get('data')
+        if data:
+            filename = f"qr_{datetime.now().strftime('%Y%m%d%H%M%S')}.png"
+            filepath = os.path.join(QR_FOLDER, filename)
+            img = qrcode.make(data)
+            img.save(filepath)
+            qr_img_path = filepath
+    return render_template('index.html', qr_path=qr_img_path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
