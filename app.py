99337db913e9ad285ab866bd3302ee2b7ac5a54d
yaaabs <brianyabut02@gmail.com>
2025-08-02 22:48:00 +0800

    Added more text and files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, send_from_directory
 import qrcode
 import os
 from datetime import datetime, timedelta
@@ -42,6 +42,27 @@ def index():
             qr_img_path = f"{QR_FOLDER}/{filename}".replace("\\", "/")
     return render_template('index.html', qr_path=qr_img_path)
 
+@app.route('/ads.txt')
+def ads():
+    return send_from_directory('.', 'ads.txt')
+
+@app.route('/robots.txt')
+def robots():
+    return '''User-agent: *
+Allow: /
+Sitemap: https://ukiyo.onrender.com/sitemap.xml''', 200, {'Content-Type': 'text/plain'}
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://ukiyo.onrender.com/</loc>
+    <lastmod>2025-08-02</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>''', 200, {'Content-Type': 'application/xml'}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
